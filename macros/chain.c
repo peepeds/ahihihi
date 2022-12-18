a9f7ab5e65b2c9584b5671fe5c7e591566115d67
peepeds <111060939+peepeds@users.noreply.github.com>
2022-12-18 21:41:15 +0700

    Create chain.c

---
+++ macros/chain.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define kelompok bebas
+#define bebas  3
+#define phi 3.14
+#define min 0.1
+
+
+int main(){
+    double jari ;
+    double luas ;
+    printf("Tugas ini dikerjakan secara berkelompok, nama kelompok kami adalah bebas ,bebas terdiri atas %d orang\n",kelompok);
+    printf("kami akan mengerjakan tugas berupa menghitung luas lingkaran\n");
+    printf("Masukkan jari - jari lingkaran : ");
+    scanf("%lf",&jari);
+    if(jari < min){
+        printf("tidak valid\n");
+    }
+    luas = phi * jari * jari ;
+    printf("Luas lingkaran adalah : %.2lf\n",luas);
+    return 0;
+}
