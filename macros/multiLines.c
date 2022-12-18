940747dadca123b72693a7bfd1c53dd8072103a1
peepeds <111060939+peepeds@users.noreply.github.com>
2022-12-18 22:00:05 +0700

    Create multiLines.c

---
+++ macros/multiLines.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define kelompok bebas
+#define bebas  3
+#define phi 3.14
+#define min 0.1
+#define cetak(luas){\
+    printf("Luas lingkaran "); \
+    printf("adalah %.2lf \n", luas); \
+}
+
+int main(){
+    double jari , i;
+    double luas ;
+   
+    printf("Tugas ini dikerjakan secara berkelompok, nama kelompok kami adalah bebas ,bebas terdiri atas %d orang\n",kelompok);
+    printf("Anggota kelompok kami adalah : \n");
+   
+    printf("kami akan mengerjakan tugas berupa menghitung luas lingkaran\n");
+    printf("Masukkan jari - jari lingkaran : ");
+    scanf("%lf",&jari);
+    if(jari < min){
+        printf("tidak valid\n");
+    }
+    luas = phi * jari * jari ;
+    cetak(luas);
+    return 0;
+}
